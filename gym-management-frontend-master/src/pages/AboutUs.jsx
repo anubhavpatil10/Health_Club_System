fbd346282a36a0ea213e97f1d71d2ea2fede42e9
Anubhav <anubhavppatil10@gmail.com>
2023-08-30 12:23:44 +0530

    abc

---
+++ gym-management-frontend-master/src/pages/AboutUs.jsx
@@ -0,0 +1,39 @@
+const AboutUs = () => {
+  return (
+    <div className="text-color-4 ms-5 me-5 mr-5 mt-3">
+      <div className="text-color-1 mt-4">
+        <h5>
+          <i>WHO WE ARE?</i>
+        </h5>
+      </div>
+      <div className="mt-3">
+        The company itself is a very successful company. Most
+         the inflexibility, the inconveniences which, even if they are advantages, result from the repudiation
+         but none of them hates the pleasures of these labors
+         let him flee with the mere flight of the present option, and of those things! Providence and the like
+         but none of the accusers. Blinded by the truth, he is bound by his right to them
+         the troubles of the architect with some pleasure, nothing, something will happen to the fault
+         services or! It prevents him from seeking, he hates, he is held in error, of these
+         They don't know what they owe you. He will criticize, because Where neither
+         the mistake of rejecting flight? She praises the troubles of those with wise duties
+         But are there ways to receive them? But to some, others are to be refused
+         the mistake of getting the most out of these things. They do not know the smallest details
+         sorrow! But the pleasures of things by the law of the offices he wills to some
+         good times Let him be from his troubles, let him flee from pains,
+         for in times of convenience, with just free, great relief, which
+         result! The least of these advantages is to be rejected here
+         and pain He seeks to provide for the benefits of forgiveness and the like
+         I will explain all of them very often, pleasure, here pleasures are bred
+         let him flee, choosing pains rather than desire to receive greater flexibility
+         what kind of work does he take? No, please. Let him seek pleasure, not the architect
+         from those who praise are the smallest ways to be at the times they are at fault, to be rejected
+         some never entirely reason for the pleasure of flight.
+         We can choose any of them as a pardon, please who the body!
+         thank you
+      </div>
+      
+    </div>
+  );
+};
+
+export default AboutUs;
